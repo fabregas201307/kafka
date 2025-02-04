3db618343d5cc0f9d93a196f4b6c5f5a3314a1ad
ody <odykai2009@gmail.com>
2025-02-04 17:57:26 -0500

    tweak docker and start script

--- docker/start-kafka.sh
+++ docker/start-kafka.sh
@@ -6,7 +6,7 @@ $KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.propertie
 # Configure Kafka broker settings
 cat <<EOF > $KAFKA_HOME/config/server.properties
 broker.id=1
-log.dirs=/tmp/kafka-logs
+log.dirs=/var/lib/kafka/logs
 zookeeper.connect=localhost:2181
 listeners=PLAINTEXT://0.0.0.0:9092
 EOF
